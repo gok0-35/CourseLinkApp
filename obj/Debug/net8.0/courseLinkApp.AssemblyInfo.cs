cf0874e999aed0b6a5d2acdbee8deefe60386928
Gökmen Soysal <gokmensoysal@gmail.com>
2025-11-04 23:44:14 +0300

    add README

--- obj/Debug/net8.0/courseLinkApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/courseLinkApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("courseLinkApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e894230df7029a4caac305c355bfcd406a847e50")]
 [assembly: System.Reflection.AssemblyProductAttribute("courseLinkApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("courseLinkApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
